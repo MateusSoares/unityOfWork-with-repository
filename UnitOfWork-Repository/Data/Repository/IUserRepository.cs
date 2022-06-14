2f9838594a69ef8bb43cdb9a5f0e9cb2641aeb56
Mateus Soares <fvs.mateus@gmail.com>
2022-06-13 23:37:17 -0300

    Repository pattern

---
+++ UnitOfWork-Repository/Data/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UnitOfWork_Repository.Data.Model;
+
+namespace UnitOfWork_Repository.Data.Repository
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        void Edit(User user);
+        User Find(int id);
+        void Remove(User user);
+    }
+}
---
+++ UnitOfWork-Repository/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UnitOfWork_Repository.Data.Context;
+using UnitOfWork_Repository.Data.Model;
+using UnitOfWork_Repository.Data.Repository;
+
+namespace UnitOfWorkWithRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var servicesProvider = serviceCollection.BuildServiceProvider();
+
+            var userRepository = servicesProvider.GetService<IUserRepository>();
+
+            var user = new User
+            {
+                Name = "User",
+                Email = "user@user.com",
+                Id = 1
+            };
+
+            userRepository?.Add(user);
+
+            var userCreated = userRepository?.Find(1);
+            Console.WriteLine(userCreated?.Name);
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>()
+                .AddDbContext<DatabaseContext>(opt => opt.UseInMemoryDatabase("fictDatabase"));
+            
+        }
+    }
+}
